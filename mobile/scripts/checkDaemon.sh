51754443578930bf7252d5cc0791af1aef48d770
Justin Pincar <jpincar@admob.com>
2010-04-29 16:33:14 -0700

    changing email for pager

--- mobile/scripts/checkDaemon.sh
+++ mobile/scripts/checkDaemon.sh
@@ -4,5 +4,5 @@ if ps aux | grep -v grep | grep java | grep Daemon  > /dev/null
 then
     :
 else
-    echo "AdWhirl Daemon is down" | mail -s "**PROBLEM - AdWhirl Daemon is CRITICAL" page-adwhirl@admob-moat.pagerduty.com #page-adwhirl@yell.admob.com
+    echo "AdWhirl Daemon is down" | mail -s "**PROBLEM - AdWhirl Daemon is CRITICAL" youremail@yourdomain.com
 fi
